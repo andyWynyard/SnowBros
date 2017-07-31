fb22fa7f8b3475a86d173fbeeb0a9b12606b15fa
TravisWay <travis_sti@yahoo.com>
2017-07-31 14:13:54 -0600

    updated database

--- snowbros.sql
+++ snowbros.sql
@@ -32,25 +32,41 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `trips`
+-- Table `destination`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trips` (
+CREATE TABLE IF NOT EXISTS `destination` (
   `id` INT NOT NULL,
-  `title` VARCHAR(45) NULL,
-  `number_seats` INT NULL,
-  `destination` VARCHAR(45) NULL,
-  `point_of_origin` VARCHAR(45) NULL,
-  `date` DATETIME NULL,
-  `point_of_return` VARCHAR(45) NULL,
-  `description` VARCHAR(45) NULL,
+  `name` VARCHAR(300) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `user_trips`
+-- Table `trip`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `user_trips` (
+CREATE TABLE IF NOT EXISTS `trip` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(45) NULL,
+  `number_seats` INT NULL,
+  `point_of_origin` VARCHAR(45) NULL,
+  `date` DATETIME NULL,
+  `point_of_return` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL,
+  `destination_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `dest_idx` (`destination_id` ASC),
+  CONSTRAINT `dest`
+    FOREIGN KEY (`destination_id`)
+    REFERENCES `destination` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_trip`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user_trip` (
   `user_id` INT NOT NULL,
   `trip_id` INT NOT NULL,
   `id` INT NOT NULL,
@@ -64,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `user_trips` (
     ON UPDATE NO ACTION,
   CONSTRAINT `trip_id`
     FOREIGN KEY (`trip_id`)
-    REFERENCES `trips` (`id`)
+    REFERENCES `trip` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -76,12 +92,12 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `extra_curr` (
   `trip_id` INT NULL,
   `name` VARCHAR(45) NULL,
-  `id` INT NULL,
-  PRIMARY KEY (`name`, `id`),
+  `id` INT NOT NULL,
+  PRIMARY KEY (`id`),
   INDEX `extras_idx` (`trip_id` ASC),
   CONSTRAINT `extras`
     FOREIGN KEY (`trip_id`)
-    REFERENCES `trips` (`id`)
+    REFERENCES `trip` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -99,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `return` (
   INDEX `trip return_idx` (`trip_id` ASC),
   CONSTRAINT `trip return`
     FOREIGN KEY (`trip_id`)
-    REFERENCES `trips` (`id`)
+    REFERENCES `trip` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -109,7 +125,7 @@ ENGINE = InnoDB;
 -- Table `user_rating`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `user_rating` (
-  `value` INT NOT NULL,
+  `value` ENUM('1', '2', '3', '4', '5') NOT NULL,
   `user_id` INT NOT NULL,
   `id` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
@@ -122,12 +138,12 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `activities`
+-- Table `activity`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `activities` (
+CREATE TABLE IF NOT EXISTS `activity` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
-  `exp` VARCHAR(45) NULL,
+  `exp` ENUM('1', '2', '3', '4', '5') NOT NULL,
   `user_id` INT NULL,
   PRIMARY KEY (`id`),
   INDEX `activities_idx` (`user_id` ASC),
@@ -145,7 +161,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `user_type` (
   `id` INT NOT NULL,
   `user_id` INT NULL,
-  `user_type` TINYINT(1) NULL,
+  `admin` TINYINT(1) NULL,
   PRIMARY KEY (`id`),
   INDEX `type_idx` (`user_id` ASC),
   CONSTRAINT `type`
@@ -155,9 +171,8 @@ CREATE TABLE IF NOT EXISTS `user_type` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE USER 'snowBro' IDENTIFIED BY 'snowBro';
+CREATE USER 'snowbros' IDENTIFIED BY 'snowbros';
 
-GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'snowBro';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
