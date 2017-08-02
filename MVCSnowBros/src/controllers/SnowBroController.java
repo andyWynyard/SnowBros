a7095135eb2903ab88e69dd52e1e22cbca5e43f7
TravisWay <travis_sti@yahoo.com>
2017-08-02 08:57:47 -0600

    started controller

---
+++ MVCSnowBros/src/controllers/SnowBroController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import data.TripDAO;
+import data.UserDAO;
+import entities.Trip;
+
+@Controller
+public class SnowBroController{
+	@Autowired
+		private TripDAO td;
+		private UserDAO ud;
+	
+		@RequestMapping(path = "searchPage.do")
+		  public String allTrips(Model model) {
+			List<Trip> trips = td.allTrips();
+			model.addAttribute("searchresults", null);
+		    model.addAttribute("allTrips", trips);
+		    return "search.jsp";
+		  }
+
+	}
+
+
+
